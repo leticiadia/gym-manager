6c52367dc871eb9c77c0a166a26d1a727fc679e1
Leticia Dias <contatoleticiadia@gmail.com>
2021-04-26 18:16:08 -0300

    file js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const routes = require('./routes')
+
+// Criando o servidor
+const server = express()
+
+server.use(express.static('public'))
+server.use(routes)
+
+server.set('view engine', 'njk') 
+
+
+
+nunjucks.configure('views', {
+    express: server,
+    autoescape: false,
+    noCache: true
+})
+
+
+// Colocando ele online
+server.listen('5001', function(){
+    console.log('Server is running')
+})
