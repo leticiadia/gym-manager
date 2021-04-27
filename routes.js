94fec749dfb6511e1c29d6408e15e146b8504630
Leticia Dias <contatoleticiadia@gmail.com>
2021-04-27 17:46:22 -0300

    modified

--- routes.js
+++ routes.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const routes  = express.Router()
 
+const instructors = require('./instructors')
+
 
 routes.get('/', function(req, res){
     return res.redirect('/instructors')
@@ -10,6 +12,12 @@ routes.get('/', function(req, res){
 routes.get('/instructors', function(req, res){
     return res.render('instructors/index')
 })
+
+routes.get('/instructors/create', function(req, res){
+    return res.render('instructors/create')
+})
+
+routes.post('/instructors', instructors.post)
 
 routes.get('/members', function(req, res){
     return res.send('members')
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const routes = require('./routes')
 const server = express()
 
 server.use(express.static('public'))
+server.use(express.urlencoded({ extended: true }))
 server.use(routes)
 
 server.set('view engine', 'njk') 
