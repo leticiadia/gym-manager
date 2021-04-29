7fdf68cd94e8f945caaaed7c8f97216b9d334e7e
Leticia Dias <contatoleticiadia@gmail.com>
2021-04-29 18:31:28 -0300

    updated file

--- routes.js
+++ routes.js
@@ -9,9 +9,7 @@ routes.get('/', function(req, res){
     return res.redirect('/instructors')
 })
 
-routes.get('/instructors', function(req, res){
-    return res.render('instructors/index')
-})
+routes.get('/instructors', instructors.index)
 
 routes.get('/instructors/create', function(req, res){
     return res.render('instructors/create')
@@ -19,7 +17,13 @@ routes.get('/instructors/create', function(req, res){
 
 routes.get('/instructors/:id', instructors.show)
 
+routes.get('/instructors/:id/edit', instructors.edit)
+
 routes.post('/instructors', instructors.post)
+
+routes.put('/instructors', instructors.put)
+
+routes.delete('/instructors', instructors.delete)
 
 routes.get('/members', function(req, res){
     return res.send('members')
