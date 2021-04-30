9adb119aadd8e7e8d71fe4c56053539af984fd05
Leticia Dias <contatoleticiadia@gmail.com>
2021-04-30 18:11:50 -0300

    updated file

--- routes.js
+++ routes.js
@@ -2,7 +2,9 @@ const express = require('express')
 
 const routes  = express.Router()
 
-const instructors = require('./instructors')
+const instructors = require('./controllers/instructors')
+
+const members = require('./controllers/members')
 
 
 routes.get('/', function(req, res){
@@ -11,9 +13,7 @@ routes.get('/', function(req, res){
 
 routes.get('/instructors', instructors.index)
 
-routes.get('/instructors/create', function(req, res){
-    return res.render('instructors/create')
-})
+routes.get('/instructors/create', instructors.create)
 
 routes.get('/instructors/:id', instructors.show)
 
@@ -25,9 +25,23 @@ routes.put('/instructors', instructors.put)
 
 routes.delete('/instructors', instructors.delete)
 
-routes.get('/members', function(req, res){
-    return res.send('members')
-})
+
+
+
+
+routes.get('/members', members.index)
+
+routes.get('/members/create', members.create)
+
+routes.get('/members/:id', members.show)
+
+routes.get('/members/:id/edit', members.edit)
+
+routes.post('/members', members.post)
+
+routes.put('/members', members.put)
+
+routes.delete('/members', members.delete)
 
 
 module.exports = routes
