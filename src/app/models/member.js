45cdfd0ba85858dd96dee01b739a49734f6dd139
Leticia Dias <contatoleticiadia@gmail.com>
2021-05-19 17:29:31 -0300

    modified

--- src/app/models/member.js
+++ src/app/models/member.js
@@ -21,8 +21,9 @@ module.exports = {
                 email, 
                 blood,
                 weight,
-                height
-            ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+                height,
+                instructor_id
+            ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
             RETURNING id
 
         `
@@ -35,7 +36,8 @@ module.exports = {
             data.email,
             data.blood,
             data.weight,
-            data.height
+            data.height,
+            data.instructor
         ]
 
         db.query(query, values, function(err, results){
@@ -46,7 +48,14 @@ module.exports = {
     },
 
     find(id, callback){
-        db.query(`SELECT * FROM members WHERE id = $1`, [id], function(err, results){
+        db.query(
+            `
+            SELECT members.*, 
+            instructors.name AS instructor_name 
+            FROM members
+            LEFT JOIN instructors ON (members.instructor_id = instructors.id)
+            WHERE members.id = $1
+            `, [id], function(err, results){
             if(err) throw `Database Error! ${err}`
 
             callback(results.rows[0])
@@ -63,8 +72,9 @@ module.exports = {
                 email=($5),
                 blood=($6),
                 weight=($7),
-                height=($8)
-            WHERE id = $9
+                height=($8),
+                instructor_id=($9)
+            WHERE id = $10
         `
 
         const values = [
@@ -76,6 +86,7 @@ module.exports = {
             data.blood,
             data.weight,
             data.height,
+            data.instructor,
             data.id
         ]
 
