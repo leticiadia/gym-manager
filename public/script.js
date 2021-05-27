17cc3698adebe3d4549f31fd149e8eb59f3b7465
Leticia Dias <contatoleticiadia@gmail.com>
2021-05-27 16:54:16 -0300

    creating pagination on the frontend

--- public/script.js
+++ public/script.js
@@ -1,58 +1,76 @@
-const currentPage = location.pathname
-const menuItems = document.querySelectorAll('header .menu a')
-
-
-for(item of menuItems){
-    if(currentPage.includes(item.getAttribute('href'))){
-        item.classList.add('active')
+function menu(){
+    const currentPage = location.pathname
+    const menuItems = document.querySelectorAll('header .menu a')
+    
+    
+    for(item of menuItems){
+        if(currentPage.includes(item.getAttribute('href'))){
+            item.classList.add('active')
+        }
     }
+    
 }
+menu()
 
 function paginate(selectedPage, totalPages){
-    let pages = [], 
-    oldpage
+    let pages = [],
+    oldPage
 
     for(let currentPage = 1; currentPage <= totalPages; currentPage++){
         
         const firstAndLastPage = currentPage == 1 || currentPage == totalPages
         const pagesAfterSelectedPage = currentPage <= selectedPage + 2
         const pagesBeforeSelectedPage = currentPage >= selectedPage - 2
-        
 
         if(firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage){
             
-            if(oldpage && currentPage - oldpage > 2){
+            if(oldPage && currentPage - oldPage > 2){
                 pages.push('...')
             }
 
-            if(oldpage && currentPage - oldpage == 2){
-                pages.push(oldpage + 1)
+            if(oldPage && currentPage - oldPage == 2){
+                pages.push(oldPage + 1)
             }
             
             pages.push(currentPage)
-
-            oldpage = currentPage
+            oldPage = currentPage
         }
-    }
-
+    }   
+    
     return pages
 }
 
-const pagination = document.querySelector('.pagination')
-const page = +pagination.dataset.page
-const total = +pagination.dataset.total
-const pages = paginate(page, total)
+function createPagination(pagination){
+    const filter = pagination.dataset.filter
+    const page = +pagination.dataset.page
+    const total = +pagination.dataset.total
+    const pages = paginate(page, total)
 
-let elements = ''
+    let elements = ''
 
-for(let page of pages){
-    if(String(page).includes('...')){
-        elements += `<span>${page}</span>`
+    for(let page of pages){
+
+        if(String(page).includes('...')){
+            elements += `<span>${page}</span>`
+        }
+        else{
+            if(filter){
+                elements += `<a href="?page=${page}&filter=${filter}">${page}</a>`
+            }
+            else{
+                elements += `<a href="?page=${page}">${page}</a>`
+            }
+            
+        }
+        
     }
-    else{
-        elements += `<a href="?page=${page}">${page}</a>`
-    }
-    
+
+    pagination.innerHTML = elements
 }
 
-pagination.innerHTML = elements
+const pagination = document.querySelector('.pagination')
+
+if(pagination){
+    createPagination(pagination)
+}
+
